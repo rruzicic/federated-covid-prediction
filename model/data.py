6a461e67723b9e1144b54b72040c2eeaff686032
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-05-24 20:45:34 +0200

    Set up everything for data loading

--- model/data.py
+++ model/data.py
@@ -1,5 +1,6 @@
-import numpy as np
 from typing import Tuple
+import numpy as np
+
 
 def load_data(path: str) -> Tuple[np.ndarray, np.ndarray]:
     return np.ndarray([]), np.ndarray([])
--- model/globals.py
+++ model/globals.py
@@ -1,6 +1,6 @@
 import yaml
 
-with open("config.yaml", "r", encoding="utf-8") as f:
+with open("model/config.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
 
 DATA_PATH = config["data_path"]
