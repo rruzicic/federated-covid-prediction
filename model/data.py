c952443ac43e40a270fea001ee22ac59a94b92a7
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-05-24 21:07:14 +0200

    Add data loading

--- model/data.py
+++ model/data.py
@@ -1,6 +1,19 @@
 from typing import Tuple
 import numpy as np
+import pandas as pd
 
+from model.globals import DATA_PATH
 
-def load_data(path: str) -> Tuple[np.ndarray, np.ndarray]:
-    return np.ndarray([]), np.ndarray([])
+def load_data() -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Loads the data and returns the preprocessed versions of it
+    """
+    df = pd.read_csv(DATA_PATH)
+
+    labels = df["COVID-19"].to_numpy()
+    labels = np.where(labels == 'Yes', 1, 0)
+
+    data = df.drop("COVID-19", axis=1).to_numpy()
+    data = np.where(data == 'Yes', 1, 0)
+
+    return data, labels
