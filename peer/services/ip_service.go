3c126352336d6f7564ea40a732c31cc59a440636
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-07-02 20:49:24 +0200

    Add way to remove and add peers to network

--- peer/services/ip_service.go
+++ peer/services/ip_service.go
@@ -14,7 +14,7 @@ func LoadPeerAddresses() ([]string, error) {
 
 	peersFile, err := os.ReadFile("peer_addresses.yaml")
 	if err != nil {
-		log.Println("Could not read peer addresses file. Error: ", err)
+		log.Println("Could not read peer addresses file. Error: ", err.Error())
 		return nil, err
 	}
 
@@ -25,6 +25,58 @@ func LoadPeerAddresses() ([]string, error) {
 	}
 
 	return peerAddresses, nil
+}
+
+func AddPeerAddress(address string) error {
+	peers, err := LoadPeerAddresses()
+	if err != nil {
+		log.Println("Could not load all peers. Error: ", err.Error())
+		return err
+	}
+
+	peers = append(peers, address)
+
+	yamlString, err := yaml.Marshal(peers)
+	if err != nil {
+		log.Println("Could not marshall new peer. Error: ", err.Error())
+		return err
+	}
+
+	if err := os.WriteFile("peer_addresses.yaml", yamlString, 0644); err != nil {
+		log.Println("Could not write to addresses yaml. Error: ", err.Error())
+		return err
+	}
+
+	return nil
+}
+
+func RemovePeerAddress(address string) error {
+	peers, err := LoadPeerAddresses()
+	if err != nil {
+		log.Println("Could not load all peers. Error: ", err.Error())
+		return err
+	}
+
+	var newPeers []string
+	for idx, peer := range peers {
+		if peer == address {
+			newPeers = append(peers[:idx], peers[idx+1:]...)
+			break
+		}
+	}
+
+	yamlString, err := yaml.Marshal(newPeers)
+	if err != nil {
+		log.Println("Could not marshall new peer list. Error: ", err.Error())
+		return err
+	}
+
+	if err := os.WriteFile("peer_addresses.yaml", yamlString, 0644); err != nil {
+		log.Println("Could not write to addresses yaml. Error: ", err.Error())
+		return err
+	}
+
+	return nil
 }
 
 func NumberOfPeers() (int, error) {
