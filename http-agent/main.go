94b982df08fed71b8a6bfec3de478def540c63fb
Vladimir Lunić <vladimir.lunic2000@gmail.com>
2023-07-02 10:52:32 +0200

    Delete ol http agent implementation

--- http-agent/main.go
+++ http-agent/main.go
@@ -1,17 +1,4 @@
 package main
 
-import (
-	console "github.com/asynkron/goconsole"
-	"github.com/asynkron/protoactor-go/actor"
-	"github.com/rruzicic/federated-covid-prediction/http-agent/actors"
-)
-
 func main() {
-	system := actor.NewActorSystem()
-	props := actor.PropsFromProducer(actors.NewHTTPActor)
-	childPid := system.Root.Spawn(props)
-	system.Root.Send(childPid, &actors.PlotLoss{})
-
-	// to never exit
-	_, _ = console.ReadLine()
 }
