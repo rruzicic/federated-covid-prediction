89d1f476f566a9459d63313d765ffbc6a2771ba7
Bosko Ristovic <malibolence@gmail.com>
2023-07-03 22:00:48 +0200

    add first real message, and WeightResponse struct

--- http-agent/messages/messages.go
+++ http-agent/messages/messages.go
@@ -1,6 +1,7 @@
 package messages
 
 import (
+	"encoding/json"
 	"io"
 	"log"
 	"net/http"
@@ -16,6 +17,11 @@ func MakeMatrix(numRows int, numColumns int) [][]float32 {
 	return matrix
 }
 
+type WeightsResponse struct {
+	Hidden_weights [][]float32 `json:"hidden_weights"`
+	Output_weights [][]float32 `json:"output_weights"`
+}
+
 func HelloWorld() error {
 	res, err := http.Get(pyServerAdress + "/")
 	if err != nil {
@@ -26,8 +32,30 @@ func HelloWorld() error {
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		log.Println("Could not read the response body")
+		return err
 	}
 
 	log.Println("Response from the Python server: " + string(body))
 	return nil
+}
+
+func GetRandomWeights() (WeightsResponse, error) {
+	var weightsResponse WeightsResponse
+	res, err := http.Get(pyServerAdress + "/random-weights")
+	if err != nil {
+		log.Println("Could not get random weights")
+		return weightsResponse, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Could not read the response body")
+		return weightsResponse, err
+	}
+
+	if err := json.Unmarshal(body, &weightsResponse); err != nil {
+		log.Println("Could not unmarshall response body into response structure. Error: ", err.Error())
+	}
+
+	return weightsResponse, nil
 }
