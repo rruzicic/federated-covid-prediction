4fabefe27bf032f6c398311a3f9bf612a068feee
Bosko Ristovic <malibolence@gmail.com>
2023-07-03 22:15:37 +0200

    add some more messages

--- http-agent/messages/messages.go
+++ http-agent/messages/messages.go
@@ -22,6 +22,13 @@ type WeightsResponse struct {
 	Output_weights [][]float32 `json:"output_weights"`
 }
 
+type ModelResponse struct {
+	Hidden_weights [][]float32 `json:"hidden_weights"`
+	Output_weights [][]float32 `json:"output_weights"`
+	Output         []float32   `json:"output"`
+	Loss           []float32   `json:"loss"`
+}
+
 func HelloWorld() error {
 	res, err := http.Get(pyServerAdress + "/")
 	if err != nil {
@@ -55,6 +62,71 @@ func GetRandomWeights() (WeightsResponse, error) {
 
 	if err := json.Unmarshal(body, &weightsResponse); err != nil {
 		log.Println("Could not unmarshall response body into response structure. Error: ", err.Error())
+		return weightsResponse, err
+	}
+
+	return weightsResponse, nil
+}
+
+func GetModel() (ModelResponse, error) {
+	var modelResponse ModelResponse
+	res, err := http.Get(pyServerAdress + "/model")
+	if err != nil {
+		log.Println("Could not get random weights")
+		return modelResponse, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Could not read the response body")
+		return modelResponse, err
+	}
+
+	if err := json.Unmarshal(body, &modelResponse); err != nil {
+		log.Println("Could not unmarshall response body into response structure. Error: ", err.Error())
+		return modelResponse, err
+	}
+
+	return modelResponse, nil
+}
+
+func AllPeersDone() (string, error) {
+	res, err := http.Get(pyServerAdress + "/all-peers-sent-weights")
+	if err != nil {
+		log.Println("Could not send all peers done signal. Error: ", err)
+		return res.Status, err
+	}
+
+	return res.Status, nil
+}
+
+func DoOneEpoch() (string, error) {
+	res, err := http.Get(pyServerAdress + "/one-epoch")
+	if err != nil {
+		log.Println("Could not send one epoch done signal. Error: ", err)
+		return res.Status, err
+	}
+
+	return res.Status, nil
+}
+
+func GetWeights() (WeightsResponse, error) {
+	var weightsResponse WeightsResponse
+	res, err := http.Get(pyServerAdress + "/weights")
+	if err != nil {
+		log.Println("Could not get weights")
+		return weightsResponse, err
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Println("Could not read the response body")
+		return weightsResponse, err
+	}
+
+	if err := json.Unmarshal(body, &weightsResponse); err != nil {
+		log.Println("Could not unmarshall response body into response structure. Error: ", err.Error())
+		return weightsResponse, err
 	}
 
 	return weightsResponse, nil
